1b8cdb873aaeaada636e44f9d5c40b77a2ee8638
Guilherme-07062002 <guilherme762002@gmail.com>
2023-09-17 01:23:36 -0300

    feat: Initial template home page

--- src/configs/theme/index.ts
+++ src/configs/theme/index.ts
@@ -23,35 +23,7 @@ const temaPadrao = createTheme({
         fontSize: 18,
         htmlFontSize: 16,
     },
-    components: {
-        MuiCssBaseline: {
-            styleOverrides: `
-				@font-face {
-					font-family: PF;
-					src: url(/assets/fonts/light.woff) format("woff");
-					font-weight: 300;
-					font-style: normal;
-					font-display: swap
-				}
-
-				@font-face {
-					font-family: PF;
-					src: url(/assets/fonts/regular.woff) format("woff");
-					font-weight: 400;
-					font-style: normal;
-					font-display: swap
-				}
-
-				@font-face {
-					font-family: PF;
-					src: url(/assets/fonts/bold.woff) format("woff");
-					font-weight: 700;
-					font-style: normal;
-					font-display: swap
-				}
-      		`,
-        },
-    },
+    
 });
 
 export default temaPadrao;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,59 @@
-import { Container, Typography } from "@mui/material"
+import { Container, Typography, Grid, Button, Paper } from "@mui/material";
 
+/**
+ * Renderiza a página inicial
+ */
 const Home = () => {
-    return (
-        <Container
-            component="main"
-            maxWidth={false}
-            sx={{
-                padding: { xs: 1, sm: 3 },
-                marginY: 1,
-                maxWidth: { xl: "90%" },
-            }}
+  return (
+    <Container
+      component="main"
+      maxWidth={false}
+      sx={{
+        padding: { xs: 1, sm: 3 },
+        marginY: 1,
+        maxWidth: { xl: "90%" },
+      }}
+    >
+      <Grid
+        container
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+        gap={"40vh"}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            width: "80vw",
+            height: "20vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-            <Typography>Olá Mundo</Typography>
-        </Container>
-    )
-}
+          <Typography
+            style={{
+              fontSize: "3rem",
+              textAlign: "center",
+            }}
+          >
+            Fifth of Hell
+          </Typography>
+        </Paper>
+
+        <Button variant="contained" color="error">
+          <Typography
+            style={{
+              fontSize: "2rem",
+              color: "white",
+            }}
+          >
+            Play
+          </Typography>
+        </Button>
+      </Grid>
+    </Container>
+  );
+};
 
 export default Home;
