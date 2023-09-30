8eea7baabafe31a0fbafa7e126d84f40cc49309b
Guilherme-07062002 <guilherme762002@gmail.com>
2023-09-30 12:08:48 -0300

    hotfix: option dice button cols

--- src/pages/Scenario/components/OptionsMenu/components/OptionDiceButton/index.tsx
+++ src/pages/Scenario/components/OptionsMenu/components/OptionDiceButton/index.tsx
@@ -8,7 +8,7 @@ type OptionDiceButtonProps = {
 /** Render button in options menu */
 export const OptionDiceButton = (data: OptionDiceButtonProps) => {
   return (
-    <Grid item xs={2.4}>
+    <Grid item>
       <Button variant="contained" color="error" size="large">
         {data.label}
       </Button>
