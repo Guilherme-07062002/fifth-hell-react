4a54822a18199a3b4b5263bdc6cb7b201f1959d8
Guilherme-07062002 <guilherme762002@gmail.com>
2023-09-30 01:14:50 -0300

    feat: refactor design and add scenario image

--- src/pages/Scenario/components/Image/index.tsx
+++ src/pages/Scenario/components/Image/index.tsx
@@ -3,7 +3,7 @@ import { Grid, Paper, Typography, Box } from "@mui/material";
 /** Render Image in Scenario */
 const Image = () => {
   return (
-    <Grid item xs={7}>
+    <Grid item xs={8}>
       <Paper
         elevation={3}
         sx={{
@@ -12,20 +12,20 @@ const Image = () => {
           flexDirection: "column",
           justifyContent: "center",
           backgroundColor: "#151926",
-          border: "solid 1px white",
         }}
       >
         <Typography
           textAlign={"left"}
-          sx={{ marginLeft: 3, paddingY: 2, color: "white" }}
+          sx={{ marginLeft: 3, paddingY: 1, color: "white", fontSize: "2rem" }}
         >
           Scenario
         </Typography>
         <Box
+          component="img"
+          src="/src/pages/Scenario/components/Image/assets/imgs/ForestDark.png"
           sx={{
             height: "100%",
             width: "100%",
-            backgroundColor: "grey",
           }}
         />
       </Paper>
--- src/pages/Scenario/components/OptionsMenu/index.tsx
+++ src/pages/Scenario/components/OptionsMenu/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Paper, Typography, Box, Button } from "@mui/material";
+import { Grid, Paper, Box, Button } from "@mui/material";
 
 /** Render options menu in scenario */
 const OptionsMenu = () => {
@@ -7,42 +7,73 @@ const OptionsMenu = () => {
       <Paper
         elevation={3}
         sx={{
+          paddingY: 2,
           height: "90vh",
           display: "flex",
           flexDirection: "column",
-          justifyContent: "center",
+          justifyContent: "space-around",
           alignItems: "center",
           backgroundColor: "#151926",
-          border: "solid 1px white",
         }}
       >
         <Box
           sx={{
-            backgroundColor: "grey",
+            backgroundColor: "#BF0A4C",
             height: "50%",
             width: "90%",
+            borderRadius: "10px",
           }}
         />
-        <Box
+        <Grid
+          container
           sx={{
-            backgroundColor: "green",
-            height: "10%",
             width: "90%",
+            display: "flex",
             flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
+            justifyContent: "space-around",
           }}
         >
-          <Typography sx={{ color: "white" }}>Roll: </Typography>
-          <Button
-            variant="contained"
-            color="error"
-            size="large"
-            sx={{ width: "10%" }}
-          >
-            D12
-          </Button>
-        </Box>
+          <Grid item xs={2.4}>
+            <Button
+              variant="contained"
+              color="error"
+              size="large"
+              sx={{ height: "50%" }}
+            >
+              D6
+            </Button>
+          </Grid>
+          <Grid item xs={2.4}>
+            <Button
+              variant="contained"
+              color="error"
+              size="large"
+              sx={{ height: "50%" }}
+            >
+              D8
+            </Button>
+          </Grid>
+          <Grid item xs={2.7}>
+            <Button
+              variant="contained"
+              color="error"
+              size="large"
+              sx={{ height: "50%" }}
+            >
+              D10
+            </Button>
+          </Grid>
+          <Grid item xs={2.4}>
+            <Button
+              variant="contained"
+              color="error"
+              size="large"
+              sx={{ height: "50%" }}
+            >
+              D20
+            </Button>
+          </Grid>
+        </Grid>
         <Button
           variant="contained"
           color="error"
