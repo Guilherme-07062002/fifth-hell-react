a49523902f82014487a60c06c684cf70aa783b65
Guilherme-07062002 <guilherme762002@gmail.com>
2023-09-30 16:23:26 -0300

    feat: rolling dices gif

--- src/pages/Scenario/components/OptionsMenu/components/DiceBox/index.tsx
+++ src/pages/Scenario/components/OptionsMenu/components/DiceBox/index.tsx
@@ -1,6 +1,10 @@
 import { StyledDiceBox as Box } from "./styled";
 
+type DiceBoxProps = {
+  showDices?: boolean;
+};
+
 /** Render dice box */
-export const DiceBox = () => {
-  return <Box />;
+export const DiceBox = (data: DiceBoxProps) => {
+  return <Box showDices={data.showDices} />;
 };
--- src/pages/Scenario/components/OptionsMenu/components/DiceBox/styled.ts
+++ src/pages/Scenario/components/OptionsMenu/components/DiceBox/styled.ts
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 import { Box } from "@mui/material";
 
-export const StyledDiceBox = styled(Box)`
+interface StyledDiceBoxProps {
+  showDices?: boolean;
+}
+
+export const StyledDiceBox = styled(Box)<StyledDiceBoxProps>`
   && {
     background-color: #bf0a4c;
     height: 50%;
     width: 80%;
     border-radius: 10px;
+    background-image: ${(props) =>
+      props.showDices
+        ? "url('/src/pages/Scenario/components/OptionsMenu/components/DiceBox/assets/imgs/dices.gif')"
+        : "none"};
   }
 `;
