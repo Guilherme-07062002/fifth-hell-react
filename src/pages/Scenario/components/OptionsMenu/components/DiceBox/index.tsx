da6c8be1a277c30e32aef8bbb84b144362912379
Guilherme-07062002 <guilherme762002@gmail.com>
2023-09-30 17:39:16 -0300

    feat: hide dice box in mobile

--- src/pages/Scenario/components/OptionsMenu/components/DiceBox/index.tsx
+++ src/pages/Scenario/components/OptionsMenu/components/DiceBox/index.tsx
@@ -1,4 +1,5 @@
 import { StyledDiceBox as Box } from "./styled";
+import { useMediaQuery, Theme, useTheme } from "@mui/material";
 
 type DiceBoxProps = {
   showDices?: boolean;
@@ -6,5 +7,12 @@ type DiceBoxProps = {
 
 /** Render dice box */
 export const DiceBox = (data: DiceBoxProps) => {
+  const theme = useTheme<Theme>();
+  //Use useMediaQuery to check if the screen is smaller than the tablet breakpoint
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  //If it is less than the tablet breakpoint, do not render the DiceBox
+  if (isMobile) return null;
+
   return <Box showDices={data.showDices} />;
 };
--- src/pages/Scenario/components/OptionsMenu/index.tsx
+++ src/pages/Scenario/components/OptionsMenu/index.tsx
@@ -13,4 +13,5 @@ const OptionsMenu = () => {
     </Grid>
   );
 };
+
 export default OptionsMenu;
