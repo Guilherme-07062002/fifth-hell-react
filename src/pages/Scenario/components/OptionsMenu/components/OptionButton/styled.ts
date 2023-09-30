06991b370101598da98f21418b8579d15598ddac
Guilherme-07062002 <guilherme762002@gmail.com>
2023-09-30 13:33:29 -0300

    feat: color buttons

--- src/pages/Scenario/components/OptionsMenu/components/OptionButton/styled.ts
+++ src/pages/Scenario/components/OptionsMenu/components/OptionButton/styled.ts
@@ -4,5 +4,10 @@ import { Button } from "@mui/material";
 export const StyledOptionButton = styled(Button)`
   && {
     width: 80%;
+    background-color: #59112b;
+    transition: background-color 0.5s ease;
+    &:hover {
+      background-color: #7d1a3a;
+    }
   }
 `;
