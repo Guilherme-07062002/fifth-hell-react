857678f01ca8831d228a1162175139ebbe38a0b4
Guilherme-07062002 <guilherme762002@gmail.com>
2023-10-01 01:40:52 -0300

    feat: add provider in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 import App from "./App";
 import defaultTheme from "./configs/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-
+    <Provider store={store}>
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
