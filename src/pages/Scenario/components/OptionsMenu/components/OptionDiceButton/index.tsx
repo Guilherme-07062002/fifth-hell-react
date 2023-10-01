7d72da6f150dddf9b4c7419e9f23e318aad0626e
Guilherme-07062002 <guilherme762002@gmail.com>
2023-10-01 02:22:12 -0300

    feat: roll dices on click button

--- src/pages/Scenario/components/OptionsMenu/components/OptionDiceButton/index.tsx
+++ src/pages/Scenario/components/OptionsMenu/components/OptionDiceButton/index.tsx
@@ -1,5 +1,7 @@
 import { Grid } from "@mui/material";
 import { StyledOptionDiceButton as Button } from "./styled";
+import { useDispatch } from "react-redux";
+import { setRollDices } from "@/store";
 
 type OptionDiceButtonProps = {
   label: string;
@@ -7,9 +9,17 @@ type OptionDiceButtonProps = {
 
 /** Render button in options menu */
 export const OptionDiceButton = (data: OptionDiceButtonProps) => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(setRollDices(true));
+
+    setTimeout(() => {
+      dispatch(setRollDices(false));
+    }, 3000);
+  };
   return (
     <Grid item>
-      <Button variant="contained" color="error" size="large">
+      <Button variant="contained" size="large" onClick={handleClick}>
         {data.label}
       </Button>
     </Grid>
--- src/pages/Scenario/components/OptionsMenu/index.tsx
+++ src/pages/Scenario/components/OptionsMenu/index.tsx
@@ -1,12 +1,16 @@
 import { OptionButton, OptionDiceButtonContainer, DiceBox } from "./components";
 import { StyledPaper as Paper, StyledGrid as Grid } from "./styled";
+import { useSelector } from "react-redux";
+import { RootStateType } from "@/store";
 
 /** Render options menu in scenario */
 const OptionsMenu = () => {
+  const { rollDices } = useSelector((state: RootStateType) => state.app);
+
   return (
     <Grid item xs={12} sm={4}>
       <Paper elevation={3}>
-        <DiceBox />
+        <DiceBox showDices={rollDices} />
         <OptionDiceButtonContainer />
         <OptionButton label="Next" />
       </Paper>
