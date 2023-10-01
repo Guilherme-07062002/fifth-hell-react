60883cd85b6aff5fd572b8303468df7295a25a1c
Guilherme-07062002 <guilherme762002@gmail.com>
2023-10-01 00:56:09 -0300

    feat: add reduxjs/toolkit & create state.ts

---
+++ src/store/state.ts
@@ -0,0 +1,41 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoading: false,
+  rollDices: false,
+  currentStage: 0,
+  stages: {
+    0: {
+      title: "Forest",
+      description: "You are in the forest",
+    },
+    1: {
+      title: "Forest Again",
+      description: "You are in the forest again",
+    },
+  },
+};
+
+const appSlice = createSlice({
+  name: "app",
+  initialState,
+  reducers: {
+    setData: (state, action) => {
+      return {
+        ...state,
+        ...action.payload,
+      };
+    },
+  },
+});
+
+export const { setData } = appSlice.actions;
+
+export const store = configureStore({
+  reducer: {
+    app: appSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
