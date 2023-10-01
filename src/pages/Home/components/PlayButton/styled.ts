641108a032dbe358abd643e4e237c144d80999aa
Guilherme-07062002 <guilherme762002@gmail.com>
2023-10-01 15:56:47 -0300

    feat: alagard font in play button

--- src/pages/Home/components/PlayButton/styled.ts
+++ src/pages/Home/components/PlayButton/styled.ts
@@ -1,6 +1,7 @@
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import Alagard from "./assets/fonts/Alagard.ttf";
 
 export const StyledButton = styled(Button)`
   && {
@@ -14,5 +15,13 @@ export const StyledLink = styled(Link)`
     color: #ffffff;
     text-decoration: none;
     font-size: 2rem;
+
+    font-family: "Alagard", sans-serif;
+  }
+  @font-face {
+    font-family: "Alagard";
+    src: url(${Alagard}) format("truetype");
+    font-weight: normal;
+    font-style: normal;
   }
 `;
