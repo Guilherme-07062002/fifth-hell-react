008718c6343c189ea3f97ec61561caeca054beda
Guilherme-07062002 <guilherme762002@gmail.com>
2023-10-04 02:50:13 -0300

    fix: dice gif reproduction

--- src/pages/Scenario/components/OptionsMenu/components/DiceBox/index.tsx
+++ src/pages/Scenario/components/OptionsMenu/components/DiceBox/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { StyledDiceBox as Box, StyledDiceValue as Typography } from "./styled";
 import { useSelector } from "react-redux";
 import { RootStateType } from "@/store";
@@ -7,10 +8,22 @@ export const DiceBox = () => {
   const { rollDices, diceValue } = useSelector(
     (state: RootStateType) => state.app
   );
-  const backgroundImage = rollDices
-    ? "url('https://github.com/Guilherme-07062002/fifth-hell-react/blob/main/src/pages/Scenario/components/OptionsMenu/components/DiceBox/assets/imgs/dice4.gif?raw=true')"
-    : "none";
-  const backgroundColor = rollDices ? "transparent" : "#0F121C";
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setImageLoaded(true);
+    };
+    img.src =
+      "https://github.com/Guilherme-07062002/fifth-hell-react/blob/main/src/pages/Scenario/components/OptionsMenu/components/DiceBox/assets/imgs/dice4.gif?raw=true";
+  }, []);
+
+  const backgroundImage =
+    rollDices && imageLoaded
+      ? "url('https://github.com/Guilherme-07062002/fifth-hell-react/blob/main/src/pages/Scenario/components/OptionsMenu/components/DiceBox/assets/imgs/dice4.gif?raw=true')"
+      : "none";
+  const backgroundColor = rollDices && imageLoaded ? "transparent" : "#0F121C";
 
   return (
     <Box style={{ backgroundImage, backgroundColor }}>
